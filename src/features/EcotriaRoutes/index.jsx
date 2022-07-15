a1622d018c6435c77859f9e4aa52b684bd1ea2d5
Roderick <rjrr507@gmail.com>
2022-07-15 02:01:13 -0500

    eslint and prettier config

--- src/features/EcotriaRoutes/index.jsx
+++ src/features/EcotriaRoutes/index.jsx
@@ -3,19 +3,19 @@ import Home from "../../pages/Home";
 import App from "../../App";
 
 const EcotriaRoutes = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<Home />} />
-				</Route>
-				<Route path="auth" element={<div>Auth</div>}>
-					<Route path="login" element={<div>Login</div>} />
-					<Route path="register" element={<div>Register</div>} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="auth" element={<div>Auth</div>}>
+          <Route path="login" element={<div>Login</div>} />
+          <Route path="register" element={<div>Register</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default EcotriaRoutes;
