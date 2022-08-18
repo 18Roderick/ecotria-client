656f2f4803b6cb371c92e4fdf83bc0e03140b50a
roderick romero <rjrr@gmail.com>
2022-08-18 07:14:27 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 /**
  * @type {import('vite').UserConfig}
  */
-export default defineConfig({
-	server: {
-		open: true,
-	},
-	plugins: [react()],
-});
+
+export default ({ mode }) => {
+  return defineConfig({
+    server: {
+      open: mode !== "production" ? true : false,
+    },
+    plugins: [react()],
+  });
+};
