ea1e639b40b1881f90dd220d0554fbd31f50a313
roderick romero <rjrr@gmail.com>
2022-08-18 07:18:52 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -12,5 +12,8 @@ export default ({ mode }) => {
       open: mode !== "production" ? true : false,
     },
     plugins: [react()],
+    preview: {
+      port: 3000,
+    },
   });
 };
