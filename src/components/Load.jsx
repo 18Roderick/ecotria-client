64b6b17a3bfa0899e76ec7668bfee098bff15160
roderick romero <rjrr@gmail.com>
2022-12-30 18:49:19 -0500

    ajustes: deploy

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -1,17 +1,13 @@
-import React,{Component} from 'react';
-
-class Load extends Component {
-    render() {
-        return (
-            <div className="card w-100 text-center shadow-xss rounded-xxl border-0 p-4 mb-3 mt-3">
-                <div className="snippet mt-2 ms-auto me-auto" data-title=".dot-typing">
-                    <div className="stage">
-                        <div className="dot-typing"></div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+const Load = () => {
+  return (
+    <div className="card w-100 text-center shadow-xss rounded-xxl border-0 p-4 mb-3 mt-3">
+      <div className="snippet mt-2 ms-auto me-auto" data-title=".dot-typing">
+        <div className="stage">
+          <div className="dot-typing"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Load;
--- src/features/EcotriaRoutes/index.jsx
+++ src/features/EcotriaRoutes/index.jsx
@@ -1,41 +1,46 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../../pages/Home.page";
-//import App from "../../App";
-import Auth from "../../pages/Auth.page";
-import SignUpPage from "../../pages/SignUp.page";
-
-import UserProducts from "../../pages/user/Products";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import { AuthProvider, PrivateRoute } from "../../context/ContextAuth";
 
+import Load from "../../components/Load";
+
+const Home = lazy(() => import("../../pages/Home.page"));
+const Auth = lazy(() => import("../../pages/Auth.page"));
+const SignUpPage = lazy(() => import("../../pages/SignUp.page"));
+const UserProducts = lazy(() => import("../../pages/user/Products"));
+
 const EcotriaRoutes = () => {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="products" element={<div> Products </div>} />
-          <Route path="auth">
-            <Route path="login" element={<Auth />} />
-            <Route path="register" element={<SignUpPage />} />
-          </Route>
-          <Route path="user" element={<Home />}>
-            <Route path="productos" element={<UserProducts />} />
-          </Route>
-          <Route
-            path="admin"
-            element={
-              <PrivateRoute>
-                <div>Admin </div>
-              </PrivateRoute>
-            }
-          ></Route>
-          <Route path="*" element={<div>404</div>} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <Suspense fallback={<Load />}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route index element={<Home />} />
+            </Route>
+            <Route path="products" element={<div> Products </div>} />
+            <Route path="auth">
+              <Route path="login" element={<Auth />} />
+              <Route path="register" element={<SignUpPage />} />
+            </Route>
+            <Route path="user" element={<Home />}>
+              <Route path="productos" element={<UserProducts />} />
+            </Route>
+            <Route
+              path="admin"
+              element={
+                <PrivateRoute>
+                  <div>Admin </div>
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route path="*" element={<div>404</div>} />
+          </Routes>
+        </AuthProvider>
+        <Outlet></Outlet>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
